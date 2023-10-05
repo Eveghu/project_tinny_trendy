78451d9c2d71d54c2e0bddf351b96e14472a2d14
Eveghu <eveguther546@gmail.com>
2023-10-05 15:39:19 -0600

    Models,Controllers and Migrations

---
+++ app/Http/Controllers/DressesController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DressesController extends Controller
+{
+    //
+}
---
+++ app/Models/Sets.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sets extends Model
+{
+    use HasFactory;
+}
